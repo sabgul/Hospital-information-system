4d73bfa633ef5e80e6e4ee59d1ea34d7f2c9c4b2
petrmiculek <petr.miculek@gmail.com>
2020-11-26 22:22:12 +0100

    reverted user manager deletion

--- backend/hospitalis/models.py
+++ backend/hospitalis/models.py
@@ -1,6 +1,6 @@
 from django.db import models, transaction
 from django.utils import timezone
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager, UserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager, UserManager as DefaultBaseUser
 
 import datetime
 
@@ -12,6 +12,37 @@ GENDER = [
     (FEMALE, 'Female'),
     (OTHER, 'Other'),
 ]
+
+
+class UserManager(BaseUserManager):
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Creates and saves a User with the given email,and password.
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+        try:
+            with transaction.atomic():
+                user = self.model(email=email,
+                                  username=email,
+                                  **extra_fields)
+                user.set_password(password)
+                user.save(using=self._db)
+                return user
+        except:
+            raise
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self._create_user(email, password=password, **extra_fields)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
