ccb6a052f67aab86dc374cd7ebc0a9217c40bdcc
petrmiculek <petr.miculek@gmail.com>
2020-11-28 02:24:12 +0100

    browsable api fix (remove eventually)

--- backend/hospis/settings.py
+++ backend/hospis/settings.py
@@ -130,6 +130,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication'  # todo remove before submitting project
     )
 }
 
